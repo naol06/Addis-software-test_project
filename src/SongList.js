73c71ba3b633a65011ebd06b445351c65c6250ca
naol06 <118481867+naol06@users.noreply.github.com>
2024-01-30 10:20:18 -0800

    last

--- src/SongList.js
+++ src/SongList.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { addToFav, updateData } from './Ridux/actionManager'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 function SongList({id,song,singer,songDescription,rating }) {
     const dispatch=useDispatch()
     const navigate=useNavigate()
+    const [data, setdata] = useState(true);
+    const pro=useSelector(state=>state.favData);
+    console.log(pro);
     const handleDelete=(id,singer)=>{
       const conf=window.confirm(`Do you Want to Delete ${singer}'s Song `)
       if(conf){
@@ -16,8 +19,21 @@ window.location.reload()
         })
         .catch(err=>console.log(err))
       }
-     
     }
+    const favid=id
+    useEffect(() => {
+      for (let i = 0; i < pro.length; i++) {
+        if(pro[i].items.id===favid){
+          setdata(false)
+        }
+        
+      }
+       }, [pro]);
+       const alertmanage=()=>{
+alert("This Song is already added to Favority")
+
+       }
+
     return (
         <div className='col-lg-4  col-md-6'>
         <div className='Song '>
@@ -38,7 +54,7 @@ window.location.reload()
        <div className='d-flex justify-content-center'>
        <button type="button" onClick={()=>dispatch(updateData({song,singer,songDescription,rating ,id})) && navigate('/view')} className="btn btn-primary">View</button>
        <button type="button" onClick={()=>dispatch(updateData({song,singer,songDescription,rating ,id})) && navigate(`/update/${id}`)} className="btn btn-info mx-2">update</button>
-       <button type="button" onClick={()=>dispatch(addToFav({song,singer,songDescription,rating ,id}))} className="btn btn-secondary mx-2">+ favority</button>
+       {data? <button type="button" onClick={()=>dispatch(addToFav({song,singer,songDescription,rating ,id}))} className="btn btn-dark mx-2">+ favority</button>:<button type="button" onClick={alertmanage}  className="btn btn-secondary mx-2">+ favority</button>}
        <button type="button" onClick={()=>handleDelete(id,singer)} className="btn btn-danger">Delete</button>
        </div>
         </div>
