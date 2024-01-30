8b553f139dfda7af6d3214b7771ef1f3889748f3
naol06 <118481867+naol06@users.noreply.github.com>
2024-01-30 13:50:33 -0800

    last push

--- src/ReadSong.js
+++ src/ReadSong.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToFav, updateData } from './Ridux/actionManager';
@@ -7,7 +7,22 @@ function ReadSong() {
     const data= useSelector(state=>state.updateData)
     const dispatch=useDispatch()
     const navigate=useNavigate()
-    console.log(data);
+    const [datafav, setdatafav] = useState(true);
+    const pro=useSelector(state=>state.favData);
+   
+    const favid=data.id
+    useEffect(() => {
+      for (let i = 0; i < pro.length; i++) {
+        if(pro[i].items.id===favid){
+          setdatafav(false)
+        }
+        
+      }
+       }, [pro]);
+       const alertmanage=()=>{
+alert("This Song is already added to Favority")
+
+       }
     return (
         <div className='d-flex justify-content-center align-items-center  vh-100'>
        {data? <div  className='border p-4 w-75 ' >
@@ -28,7 +43,7 @@ function ReadSong() {
             <p>{data.songDescription}</p>
            <div className='d-flex justify-content-center '>
            <button type="button" onClick={()=>dispatch(updateData({song:data.song,singer:data.singer,songDescription:data.songDescription,rating:data.rating ,image:data.image,id:data.id})) && navigate('/update')} className="btn btn-info mx-2">update</button>
-            <button type="button" onClick={()=>dispatch(addToFav({song:data.song,singer:data.singer,songDescription:data.songDescription,rating:data.rating ,image:data.image,id:data.id}))} className="btn btn-warning mx-2">+ favority</button>
+           {datafav? <button type="button" onClick={()=>dispatch(addToFav({song:data.song,singer:data.singer,songDescription:data.songDescription,rating:data.rating ,image:data.image,id:data.id}))} className="btn btn-warning mx-2">+ favority</button>:<button type="button" onClick={alertmanage}  className="btn btn-secondary mx-2">+ favority</button>}
             </div>
           </Card.Body>
           
